55dc91348bead5000e0f52b14d09fe4cef477e11
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-11-14 09:47:25 +0100

    Number and booleans.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 "use strict";
 //String
 var myName = 'Jovan';
-myName = '5';
+//myName = 5;
+//number
+var myAge = 27;
+//myAge='Max'
+myAge = 10e5;
+//boolean
+var hasHobbies = true;
+hasHobbies = 1;
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
 //String
 let myName = 'Jovan';
 //myName = 5;
+
+//number
+let myAge = 27;
+//myAge='Max'
+myAge = 10e5;
+
+//boolean
+let hasHobbies = true;
+//hasHobbies = 1;
