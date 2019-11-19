bea30f4901bdf0ca3e127ca96ba915e5ac682de9
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-11-19 07:14:32 +0100

    Namespaces

---
+++ app.ts
@@ -0,0 +1,5 @@
+import { calculateCircumference } from './math/circle';
+import { rectangleCircumference } from './math/rectangle';
+
+console.log(calculateCircumference(10));
+console.log(rectangleCircumference(10, 20));
---
+++ math/rectangle.ts
@@ -0,0 +1,3 @@
+export function rectangleCircumference(a: number, b: number) {
+    return a * b;
+}
