33067bbe887623228f3e226324a5e279a9e14617
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-11-19 09:49:29 +0100

    The basics about interfaces.

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
+interface NamedPerson {
+    name: string;
+}
+
 console.log('INTERFACES');
+
+function greet(person: NamedPerson): void {
+    console.log(`Hello ${person.name}`);
+}
+
+const person = {
+    name: 'Jovan',
+    age: '24',
+};
+greet(person);
