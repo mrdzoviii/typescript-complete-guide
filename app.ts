1bd6fe521d06a2836f944236a78c858188f0114c
Jovan Danilovic <jovan.danilovic@telegroup-ltd.com>
2019-11-19 11:37:05 +0100

    Using interfaces with classes.

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ interface NamedPerson {
     name: string;
     age?: number;
 
+    greet(lastName: string): void;
+
     [propName: string]: any;
 }
 
@@ -9,11 +11,32 @@ console.log('INTERFACES');
 
 function greet(person: NamedPerson): void {
     console.log(`Hello ${person.name}`);
+    console.log(person.greet('MAXX'));
 }
 
 const person = {
     name: 'John',
     hobbies: ['Cooking', 'Sports'],
+    greet(lastName: string) {
+        console.log(`Hi, I am ${name} ${lastName}`);
+    },
 };
 greet(person);
-greet({ name: 'John', age: 25 });
+greet({
+    name: 'John',
+    age: 25,
+    greet(lastName: string) {
+        console.log(`Hi, I am ${name} ${lastName}`);
+    },
+});
+
+class Person implements NamedPerson {
+    constructor(public name: string) {}
+
+    greet(lastName: string) {
+        console.log(lastName);
+    }
+}
+
+const myPerson = new Person('Jovan');
+greet(myPerson);
